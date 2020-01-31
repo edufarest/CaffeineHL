1a407a28edde5b9fa8717b85e684760a66bb7945
Eduardo Fares <edufarest@gmail.com>
2020-01-30 22:35:49 -0500

    Add stubs

---
+++ models/DrinkRecord.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+export default class DrinkRecord {
+    constructor() {}
+
+    createDrinkRecord(drink, startTime) {
+        let drinkRecordId;
+
+        // Create drink record and return id
+
+        return drinkRecordId;
+    }
+
+    listDrinkRecords(start, end = Math.floor(Date.now() / 60000)) {
+        // Get all records from given time range
+    }
+
+    getDrinkRecord(id) {
+        let drinkRecord;
+
+        // Get drink from db
+
+        return drinkRecord;
+    }
+}
